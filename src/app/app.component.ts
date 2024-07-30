5121ab021fa087a453419c329d010551e4568eed
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-07-29 23:56:11 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductService } from './services/product.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet], // Solo componentes, directivas o pipes aquí
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Corregido a styleUrls
 })
-export class AppComponent {
-  title = 'proyect-noSQL';
+
+
+export class AppComponent implements OnInit {
+
+  public data: any;
+
+  // Inyección de servicio en el constructor
+  constructor(private API: ProductService) {}
+
+  ngOnInit(): void {
+    // Llamada al servicio para obtener productos
+    this.API.getProducts().subscribe(
+      response => {
+        this.data = response;
+        console.log(this.data);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
